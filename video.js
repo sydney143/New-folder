a8a76c4d010ffb01fd14ea2cbd9baf39e7505957
Sydney Evans <sydneye143@gmail.com>
2021-04-02 15:43:43 -0500

    play and pause

---
+++ video.js
@@ -0,0 +1,15 @@
+console.log("script is running")
+
+var x=document.querySelector("#video")
+console.log(x);
+function playvideo() {
+x.play();
+console.log("hover")
+}
+
+function pausevideo() {
+x.pause();
+}
+
+
+
